7f1a96d9df3c243d671dae9a8b6f061d33f81138
Cole McCoy <cole.mccoy41@hotmail.com>
2020-11-02 02:17:11 -0600

    adjusted config

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
-export const baseUrl = process.env.REACT_APP_BASEURL || `https://shlack-backend.herokuapp.com/api`;
-export const socketUrl = process.env.REACT_APP_BASEURL || `https://shlack-backend.herokuapp.com`;
+export const baseUrl = process.env.REACT_APP_BASEURL || `https://localhost:8000/api`;
+export const socketUrl = process.env.REACT_APP_BASEURL || `https://localhost:8000`;
