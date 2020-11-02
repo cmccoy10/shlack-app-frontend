851dffbc493579cfb4979d59d27bb130a20aced3
Cole McCoy <cole.mccoy41@hotmail.com>
2020-11-02 03:54:02 -0600

    fixed front end(working)

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
-export const baseUrl = process.env.REACT_APP_BASEURL || `https://localhost:8000/api`;
-export const socketUrl = process.env.REACT_APP_BASEURL || `https://localhost:8000`;
+export const baseUrl = process.env.REACT_APP_BASEURL || `http://localhost:8000/api`;
+export const socketUrl = process.env.REACT_APP_BASEURL || `http://localhost:8000`;
