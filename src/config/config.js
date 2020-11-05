1597cf6386fe46d64efc5ae461198bf2798ff36e
Cole McCoy <cole.mccoy41@hotmail.com>
2020-11-04 21:26:15 -0600

    fixed api environment variable

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
 export const baseUrl = process.env.REACT_APP_BASE_URL || `http://localhost:8000`;
-export const apiUrl = process.env.REACT_APP_BASE_URL || `${baseUrl}/api`;
+export const apiUrl = `${baseUrl}/api`;
