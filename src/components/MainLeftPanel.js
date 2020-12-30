edf8edefff6db93d3de49bfb78a55bad84715aa1
Cole McCoy <cole.mccoy41@hotmail.com>
2020-12-30 17:46:51 -0600

    Added social links

--- src/components/MainLeftPanel.js
+++ src/components/MainLeftPanel.js
@@ -4,12 +4,18 @@ import { AppBar, Toolbar, IconButton, Typography, Box, Avatar, Grid } from "@mat
 import { useSelector } from 'react-redux';
 import ChannelList from './ChannelList';
 import GroupList from "./GroupList";
+import { GitHub, LinkedIn } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
+
 
 const useStyles = makeStyles((theme) => ({
   sidePanel: {
     background: "#303E4D",
     width: "100%",
-    height: "100%"
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
   },
   panelHeader: {
     borderTop: "thin solid #45515f",
@@ -20,18 +26,41 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: "row",
     justifyContent: "flex-start",
     alignItems: "center"
-  }
+  },
+  socialContainer: {
+    paddingLeft: ".5em",
+    paddingBottom: ".5em"
+  },
+  socialLinks: {
+      textDecoration: "none",
+      color: "white",
+      paddingRight: ".3em",
+  },
+  small: {
+    width: theme.spacing(3.8),
+    height: theme.spacing(3.8),
+  },
 }));
 
 const MainLeftPanel = () => {
   const classes = useStyles();
   return (
     <div className={classes.sidePanel} >
-      <Box className={classes.panelHeader}>
-        <Typography variant="h5" color="primary" fontWeight="800" >Shlack</Typography>
-      </Box>
-      <Box>
-        <ChannelList />
+        <Box>
+            <Box className={classes.panelHeader}>
+                <Typography variant="h5" color="primary" fontWeight="800" >Shlack</Typography>
+            </Box>
+            <Box>
+                <ChannelList />
+            </Box>
+        </Box>
+      <Box className={classes.socialContainer}>
+        <a href="https://github.com/cmccoy10" className={classes.socialLinks} >
+            <GitHub className={classes.small}/>
+        </a>
+        <a href="https://www.linkedin.com/in/cole-mccoy-20665096/" className={classes.socialLinks} >
+            <LinkedIn className={classes.small}/>
+        </a>
       </Box>
       {/* <Box>
         <GroupList />
